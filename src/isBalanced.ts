92acf37f85b15df7b2921615c4837c07945b214d
Denys Korolkov <denys@korolkov.pro>
2018-01-25 17:48:27 -0500

    +isBalancedAdvanced: fn => for different braсkets

--- src/isBalanced.ts
+++ src/isBalanced.ts
@@ -31,3 +31,27 @@ export function isBalanced(str: string): boolean {
 
 	return !count;
 }
+
+/**
+ * Takes a string and returns true or false indicating whether its curly braces are balanced.
+ * supports 3 types of braces: curly {}, square [], and round ().
+ * A string with interleaving braces should return false.
+ *
+ * @example
+ *
+ * isBalancedAdvanced('(foo { bar (baz) [boo] })') // true
+ * isBalancedAdvanced('fo{s{}s}o { bar { baz }')   // false
+ * isBalancedAdvanced('foo { (bar [baz] } )')      // false
+ *
+ * @export
+ * @param {string} str
+ * @returns {boolean}
+ */
+export function isBalancedAdvanced(str: string): boolean {
+	let prev: string = '';
+	str = str.replace(/[^{}()\[\]]/g, '');
+	while (str != prev) {
+		str = (prev = str).replace(/\{\}|\[\]|\(\)/g, '');
+	}
+	return !str.length;
+}
--- tests/unit/intermediate.ts
+++ tests/unit/intermediate.ts
@@ -1,19 +1,24 @@
 import { fibMemoization } from '../../src/fib';
+import { isBalancedAdvanced } from '../../src/isBalanced';
 
 const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
 registerSuite('Intermediate', {
-	fib: {
-		'0,1,10,13,20,30,50,100'() {
-			assert.equal(fibMemoization(0), 0);
-			assert.equal(fibMemoization(1), 1);
-			assert.equal(fibMemoization(10), 55);
-			assert.equal(fibMemoization(13), 233);
-			assert.equal(fibMemoization(20), 6765);
-			assert.equal(fibMemoization(30), 832040);
-			assert.equal(fibMemoization(50), 12586269025);
-			assert.equal(fibMemoization(100), 354224848179262000000);
-		}
+	fib() {
+		assert.equal(fibMemoization(0), 0);
+		assert.equal(fibMemoization(1), 1);
+		assert.equal(fibMemoization(10), 55);
+		assert.equal(fibMemoization(13), 233);
+		assert.equal(fibMemoization(20), 6765);
+		assert.equal(fibMemoization(30), 832040);
+		assert.equal(fibMemoization(50), 12586269025);
+		assert.equal(fibMemoization(100), 354224848179262000000);
+	},
+
+	isBalancedAdvanced() {
+		assert.equal(isBalancedAdvanced('(foo { bar (baz) [boo] })'), true);
+		assert.equal(isBalancedAdvanced('foo { bar { baz }'), false);
+		assert.equal(isBalancedAdvanced('foo { (bar [baz] } )'), false);
 	}
 });
